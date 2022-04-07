1cca0e0e5be3c9781e6acbe1b9964499ab487703
Alief Muhammad Abdillah <91062027+aliefabdillah@users.noreply.github.com>
2022-04-07 11:22:40 +0700

    Update memory_map.c

--- memory_map.c
+++ memory_map.c
@@ -4,6 +4,7 @@
 #include <stdio.h>      /* printf() */
 #include <sys/wait.h>   /*wait*/
 #include <errno.h>	    /* errno */
+#include <time.h>	    /* time */
 
 #define INTSIZE 50
 
@@ -11,41 +12,44 @@ int main(int argc, char **argv) {
         int pid;
         int proccesProd;
         int proccesCon;
+        int procces;
 
         printf("mulai \n");
-        // printf("Masukan Jumlah Proses : ");
-        // scanf("%d", &procces);
+        printf("Masukan Jumlah Proses : ");
+        scanf("%d", &procces);
         
         int size = INTSIZE * sizeof(int);
         //buat memory map
         void *addr = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
         printf("Mapped at %p\n", addr);
         int *shared = addr;
+        
         switch (pid = fork()) {
         case 0:         /*  proses anak */
                 printf("Proses anak \n");
                 //isi data di memory map
-                printf("Masukan Jumlah Proses Producer : ");
-                scanf("%d", &proccesProd);
+                // printf("Masukan Jumlah Proses Producer : ");
+                // scanf("%d", &proccesProd);
                 
-                for (int i = 0; i < proccesProd; i++) {
+                srand(time(0));
+                for (int i = 0; i < procces; i++) {
                     shared[i] = rand() % 50;
-                    printf("Producer menghasilkan angka : %d\n", shared[i]);
+                    printf("Producer %d menghasilkan angka : %d\n", i, shared[i]);
                 }
                 break;
         default:        /*  ortu */
                 printf("Proses ortu\n");
                 int pidWait,status;
+                int totalNumber = 0;
                 /* wait sampai child selesai */
                 while (pidWait = wait(&status)) {   
                     if (pidWait == pid)  /* child sukses selesai*/
                         //cetak isi memory map
-                        printf("Masukan Jumlah Proses Consumer : ");
-                        scanf("%d", &proccesCon);
-                        int totalNumber = 0;
+                        // printf("Masukan Jumlah Proses Consumer : ");
+                        // scanf("%d", &proccesCon);
                         
-                        for (int i = 0; i<proccesCon; i++) {
-                            printf("Consumer menerima angka: %d\n", shared[i]);
+                        for (int i = 0; i<procces; i++) {
+                            printf("Consumer %d menerima angka: %d\n", i, shared[i]);
                             totalNumber = totalNumber + shared[i];
                         }
                         printf("Total Number yang diterima Consumer : %d\n", totalNumber);
